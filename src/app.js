dbcbb1370fa709f02249eb04c57603b76d658d43
Le Tan <tanle1069@gmail.com>
2021-04-23 17:58:17 +0700

    Update

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+//http logger
+app.use(morgan('combined'));
+
+//template engine
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'resource/view'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.listen(port, () => {
+    console.log('Example app listening at http://localhost:${port}');
+});
